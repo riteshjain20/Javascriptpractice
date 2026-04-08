651d5743610e9e94366cc11b8819d1349a0310de
Ritesh Jain <rj4380790@gmail.com>
2026-04-08 22:50:01 +0530

    add array and function files with initial examples

---
+++ array.js
@@ -0,0 +1,63 @@
+
+
+// const city = ["Delhi", "Mumbai", "Chennai", "Kolkata"];
+// console.log(typeof city);
+
+// const city = new Array();
+// city[0] = "Delhi";
+// city[1] = "Mumbai";
+// city[2] = "Chennai";
+// city[3] = "Kolkata";
+// console.log(city);
+
+// const city = new Array("Delhi", "Mumbai", "Chennai", "Kolkata");
+// console.log(city);
+
+
+// const city = ["Delhi", "Mumbai", "Chennai", "Kolkata","Delhi", "Chennai"];
+// let length = city.length;
+// console.log(length);
+
+// let cityIndex = city.indexOf("Chennai");
+// console.log(cityIndex);
+
+// let cityLastIndex = city.lastIndexOf("Chennai");
+// console.log(cityLastIndex);
+
+// push() fn - add element at the end of array 
+// city.push("kolkata");
+// console.log(city);
+
+// pop() fn - remove last element 
+// city.pop();
+// console.log(city);
+
+// shift() fn - remove first element
+// city.shift();
+// console.log(city);
+
+// unshift() fn - add element at the beginning of array  
+// city.unshift("kolkata");
+// console.log(city);
+
+
+// const student = ["Ritesh", "Rahul", "Suresh", "Ramesh"];
+// student.sort();
+// console.log(student);
+
+// student.reverse();
+// console.log(student);
+
+
+// let age = [20, 15, 25, 10];
+// let filterage = age.filter(function(value){
+//   return value > 18;
+// })
+// console.log(filterage);
+
+
+let age = [20, 15, 25, 10];
+age.forEach(function(value){
+  console.log(value);
+})
+
---
+++ function.js
@@ -0,0 +1,26 @@
+
+// function with return
+// function addition(a,b){
+//   return a+b;
+// }
+// let add = addition(2,3);
+// console.log("Addition is " + add);
+
+
+// function with expression 
+// let funexp = function(){
+//   console.log("its function expression");
+// }
+// funexp();
+
+// let funexp = function(x,y){
+//   return x+y;
+// }
+// let result = funexp(5,6);
+// console.log(result);
+
+
+// arrow functions 
+let add = (x,y) => x+y;
+let result = add(10,20);
+console.log("Addition is " + result);
